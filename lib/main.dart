6ac7d9b711b4a947fe34f4a2ece653ad057b81f8
Juliano <kluge233@hotmail.com>
2023-03-24 15:23:18 -0300

    Created notion integration

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:cripto/repositories/conta_repository.dart';
 import 'package:cripto/repositories/favoritos_repository.dart';
 import 'package:cripto/repositories/moeda_repository.dart';
 import 'package:cripto/service/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,6 +16,8 @@ Future<void> main() async {
   await Firebase.initializeApp();
 
   await HiveConfig.start();
+
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
 
   runApp(
     MultiProvider(
